c0e5dbefb2940eacbc78ffad673d0574324cc0f9
新健 <xinjian@qunhemail.com>
2022-11-04 15:54:52 +0800

    fix

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import './App.css'
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
