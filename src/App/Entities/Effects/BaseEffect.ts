4ba0657107055570d93c537fa89e7964727bc66d
新健 <xinjian@qunhemail.com>
2022-11-25 11:48:55 +0800

    fix

--- src/App/Entities/Effects/BaseEffect.ts
+++ src/App/Entities/Effects/BaseEffect.ts
@@ -1,9 +1,10 @@
 export enum EffectStatus {
   NORMAL = "NORMAL",
   VERTIGO = "VERTIGO", //眩晕
+  TRACEBACK = "TRACEBACK", //回溯时间
 }
 export default class BaseEffect {
   public type: EffectStatus = EffectStatus.NORMAL;
   public keepTime = 0;
-  
+  public CD = 0;
 }
